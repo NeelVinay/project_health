4ea7237ffe88bbd0ee127f678aee9378f900c8d3
Neel Vinay <vinay.n@northeastern.edu>
2026-03-14 21:15:46 -0700

    Initial Commit

---
+++ FitLock/FitLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitLockApp.swift
+//  FitLock
+//
+//  Created by Neel Vinay on 14/3/26.
+//
+
+import SwiftUI
+
+@main
+struct FitLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
